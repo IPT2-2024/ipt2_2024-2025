b684076115fbe315b48b78c8dc4258b31eda9d8e
NazirBuenavidez <nazzbuenavidez@hotmail.com>
2024-11-22 22:31:29 +0800

    workspace for IPT Project

--- resources/js/components/Routers.js
+++ resources/js/components/Routers.js
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from "./Home";
-
+import Header from "./Public/Header";  // Import the Header component
 
 export default function Routers() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/*" element={<Header />} />  {/* Add the Header component here, ensuring it loads for all routes */}
             </Routes>
         </Router>
     );
