dcf7a98e2ebc1c5da33f9679834c1409880bec59
catorjalen <cator.jalen@gmail.com>
2024-11-24 15:12:51 +0800

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,9 @@
     <link rel="stylesheet" href="{{ mix('css/loader.css') }}">
     <link rel="stylesheet" href="{{ mix('css/login.css') }}">
 
+    <link href="https://cdn.jsdelivr.net/npm/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
     <link rel="icon" type="image/svg+xml" href="/../images/fsuu-icon-01.svg">
+
 </head>
 <body>
     <div id="root">
