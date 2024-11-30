7c85f8e766194d60cf6cb615aed854d98a0ce6d1
JakeM00N <159703928+JakeM00N@users.noreply.github.com>
2024-11-30 22:07:09 +0800

    replace bg add loading code

--- resources/js/components/Home.js
+++ resources/js/components/Home.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Loader from "./Loader";
+import Login from "./Login"; 
 
 export default function Home() {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        // Simulate a loading delay of 3 seconds
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 3000);
+
+        // Cleanup the timer to prevent memory leaks
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
-        <div className="home-container ">
-            <Loader />
+        <div className="home-container">
+            {isLoading ? <Loader /> : <Login />}
         </div>
     );
 }
